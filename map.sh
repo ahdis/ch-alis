002501e870c775f26e9fa257ff221b9dd46ac187
oliveregger <oliver.egger@ahdis.ch>
2025-01-06 14:14:52 +0100

    LKAAT changes

--- map.sh
+++ map.sh
@@ -1,3 +1,3 @@
 rm log/*.log
-rm LKAAT/CodeSystem-lkaat.json
+rm LKAAT/LKAAT.json
 java -jar "/Users/oegger/.vscode/extensions/yannick-lagger.vscode-fhir-tools-1.5.1/validator_cli.jar" -factory ${PWD}/factories.json -version 4.0
